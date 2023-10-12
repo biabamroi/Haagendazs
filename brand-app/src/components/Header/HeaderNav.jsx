3ba3d082b6df2f184dda94e61943a2e78410ac7e
Kai <biabamroi@gmail.com>
2023-10-12 16:40:06 +0900

    🚧

--- brand-app/src/components/Header/HeaderNav.jsx
+++ brand-app/src/components/Header/HeaderNav.jsx
@@ -11,6 +11,12 @@ const HeaderNav = () => {
     setIsopen(!isOpen);
     // isOpen === true ? setIsopen(false) : setIsopen(true);
   }
+
+  const [isOn, setIson] = useState(false);
+  const accordion = () => {
+    setIson(!isOn);
+  }
+
   return (
     <>
       <div className='header-wrap'>
@@ -23,78 +29,81 @@ const HeaderNav = () => {
       </div>
       <div className="container">
         <div className={`sidebar ${isOpen ? 'active' : ''}`}>
-            <div className="sd-header">
-              <a className="navbar-brand" href="/">
-                <img src={logo} alt="logo"/>
-              </a>
-              <div className="close" onClick={toggleSidebar}><Icon icon="lucide:list-x" height="23"/></div>
-              <div className="btn-wrap">
-                <button className="" type="button">회원가입</button>
-                <button className="" type="button">로그인</button>
-                <button className="" type="button">주문조회</button>
-              </div>
+          <div className="sd-header">
+            <a className="navbar-brand" href="/">
+              <img src={logo} alt="logo"/>
+            </a>
+            <div className="close" onClick={toggleSidebar}><Icon icon="lucide:list-x" height="23"/></div>
+            <div className="btn-wrap">
+              <button className="join" type="button">회원가입</button>
+              <button className="" type="button">로그인</button>
+              <button className="" type="button">주문조회</button>
             </div>
-            <div className="sd-body">
-              <div className="line"></div>
-              <p>
-                <span className="sd-brand">Brand</span>
-                <Icon icon="mdi:cookie-plus"/>
-                <Icon icon="mdi:cookie-minus-outline" />
-              </p>
-              <ul>
-                <span></span>
-                <li><a className="sd-link" href="/">하겐다즈 소개</a></li>
-                <li><a className="sd-link" href="/">하겐다즈 브랜드 스토리</a></li>
-                <li><a className="sd-link" href="/">하겐다즈 가치</a></li>
-                <li><a className="sd-link" href="/">하겐다즈의 품질 좋은 원료</a></li>
-              </ul>
+          </div>
+          <div className="sd-body">
             <div className="line"></div>
-              <p>
-                <span className="sd-brand">Products</span>
-                <Icon icon="mdi:cookie-plus"/>
-                <Icon icon="mdi:cookie-minus-outline" />
-              </p>
-              <ul>
-                <span>모든제품</span>
-                <li><a className="sd-link" href="/">전체보기</a></li>
-                <li><a className="sd-link" href="/">파인트</a></li>
-                <li><a className="sd-link" href="/">미니컵</a></li>
-                <li><a className="sd-link" href="/">스틱바/샌드</a></li>
-                <li><a className="sd-link" href="/">케이크&디저트</a></li>
-                <li><a className="sd-link" href="/">굿즈</a></li>
-              </ul>
-            <div className="line"></div>
-              <p>
-                <span className="sd-brand">Notice</span>
-                <Icon icon="mdi:cookie-plus"/>
-                <Icon icon="mdi:cookie-minus-outline" />
-              </p>
-              <ul>
-                <span>안내</span>
-                <li><a className="sd-link" href="/">공지사항</a></li>
-                <li><a className="sd-link" href="/">이벤트</a></li>
-                <li><a className="sd-link" href="/">매장안내</a></li>
-                <li><a className="sd-link" href="/">고객센터</a></li>
-              </ul>
-            <div className="line"></div>
-            </div>
-            <div className="sd-footer">
-              <a href="/">
-                talk
-              </a>
-              <a href="https://twitter.com/HaagenDazs_US">
-                <Icon icon="formkit:twitter" height="23" />
-              </a>
-              <a href="https://www.instagram.com/haagendazskorea/">
-                <Icon icon="entypo-social:instagram-with-circle" height="23" />
-              </a>
-              <a href="https://www.facebook.com/HaagenDazsKR/">
-                <Icon icon="formkit:facebook" height="23" />
-              </a>
-              <a href="https://www.youtube.com/c/HaagenDazsKorea/featured">
-                <Icon icon="entypo-social:youtube-with-circle" height="23" />
-              </a>
-            </div>
+            <p>
+              <span className="sd-brand">Brand</span>
+              <span className="on" onClick={accordion}>
+                <Icon className="switch" icon="mdi:cookie-plus"/>
+                <Icon className="switch" icon="mdi:cookie-minus-outline" />
+              </span>
+            </p>
+            <ul className={`accordion ${isOn ? 'on' : 'off'}`}>
+              <li><a className="sd-link" href="/">하겐다즈 소개</a></li>
+              <li><a className="sd-link" href="/">하겐다즈 브랜드 스토리</a></li>
+              <li><a className="sd-link" href="/">하겐다즈 가치</a></li>
+              <li><a className="sd-link" href="/">하겐다즈의 품질 좋은 원료</a></li>
+            </ul>
+          <div className="line"></div>
+            <p>
+              <span className="sd-brand">Products</span>
+              <span className="on" onClick={accordion}>
+                <Icon className="switch" icon="mdi:cookie-plus"/>
+                <Icon className="switch" icon="mdi:cookie-minus-outline" />
+              </span>
+            </p>
+            <ul className={`accordion ${isOn ? 'on' : 'off'}`}>
+              <li><a className="sd-link" href="/">전체보기</a></li>
+              <li><a className="sd-link" href="/">파인트</a></li>
+              <li><a className="sd-link" href="/">미니컵</a></li>
+              <li><a className="sd-link" href="/">스틱바/샌드</a></li>
+              <li><a className="sd-link" href="/">케이크&디저트</a></li>
+              <li><a className="sd-link" href="/">굿즈</a></li>
+            </ul>
+          <div className="line"></div>
+            <p>
+              <span className="sd-brand">Notice</span>
+              <span className="on" onClick={accordion}>
+                <Icon className="switch" icon="mdi:cookie-plus"/>
+                <Icon className="switch" icon="mdi:cookie-minus-outline" />
+              </span>
+            </p>
+            <ul className={`accordion ${isOn ? 'on' : 'off'}`}>
+              <li><a className="sd-link" href="/">공지사항</a></li>
+              <li><a className="sd-link" href="/">이벤트</a></li>
+              <li><a className="sd-link" href="/">매장안내</a></li>
+              <li><a className="sd-link" href="/">고객센터</a></li>
+            </ul>
+          <div className="line"></div>
+          </div>
+          <div className="sd-footer">
+            <a href="/">
+              <Icon icon="simple-icons:kakaotalk" height="30" style={{ borderRadius: '50%' }} />
+            </a>
+            <a href="https://twitter.com/HaagenDazs_US">
+              <Icon icon="formkit:twitter" height="30" />
+            </a>
+            <a href="https://www.instagram.com/haagendazskorea/">
+              <Icon icon="entypo-social:instagram-with-circle" height="30" />
+            </a>
+            <a href="https://www.facebook.com/HaagenDazsKR/">
+              <Icon icon="formkit:facebook" height="30" />
+            </a>
+            <a href="https://www.youtube.com/c/HaagenDazsKorea/featured">
+              <Icon icon="entypo-social:youtube-with-circle" height="30" />
+            </a>
+          </div>
         </div>
         <div className={`sidebar-overlay ${isOpen ? 'active' : ''}`} onClick={toggleSidebar}></div>
       </div>
