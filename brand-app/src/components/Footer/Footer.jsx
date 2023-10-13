9c30662cec07de29cff356979f24b8a7598b316a
choiseowu <chlthdud28@naver.com>
2023-10-13 13:50:35 +0900

    🚧 수정중

--- brand-app/src/components/Footer/Footer.jsx
+++ brand-app/src/components/Footer/Footer.jsx
@@ -9,32 +9,66 @@ const Footer = () => {
       <footer>
         <div className="footer-wrapper">
           <div className="footer-left">
-            <img src={footerlogo} alt="footer logo image" className="footerlogo" />
+            <img
+              src={footerlogo}
+              alt="footer logo image"
+              className="footerlogo"
+            />
             <button type="button">CONTACT US</button>
           </div>
-          <div className="footer-right" >
-            <ul>
-              <li><a href="">파인트</a></li>
-              <li><a href="">미니 컵</a></li>
-              <li><a href="">스틱바</a></li>
-              <li><a href="">스틱바 멀티팩</a></li>
-              <li><a href="">미니  스틱바</a></li>
+          <div className="footer-right">
+            <ul className="footer-list">
+              <li>
+                <a href="">파인트</a>
+              </li>
+              <li>
+                <a href="">미니 컵</a>
+              </li>
+              <li>
+                <a href="">스틱바</a>
+              </li>
+              <li>
+                <a href="">스틱바 멀티팩</a>
+              </li>
+              <li>
+                <a href="">미니 스틱바</a>
+              </li>
             </ul>
-            <ul>
-              <li><a href="">콘</a></li>
-              <li><a href="">콘 멀티백</a></li>
+            <ul className="footer-list">
+              <li>
+                <a href="">콘</a>
+              </li>
+              <li>
+                <a href="">콘 멀티백</a>
+              </li>
             </ul>
-            <ul>
-              <li><a href="">벌크 (업소용)</a></li>
-              <li><a href="">온라인 구매하기</a></li>
-              <li><a href="">업소문의</a></li>
-              <li><a href="">매장 소개</a></li>
-              <li><a href="">크리에이션 메뉴</a></li>
-              <li><a href="">아이스크림 드링크</a></li>
+            <ul className="footer-list">
+              <li>
+                <a href="">벌크 (업소용)</a>
+              </li>
+              <li>
+                <a href="">온라인 구매하기</a>
+              </li>
+              <li>
+                <a href="">업소문의</a>
+              </li>
+              <li>
+                <a href="">매장 소개</a>
+              </li>
+              <li>
+                <a href="">크리에이션 메뉴</a>
+              </li>
+              <li>
+                <a href="">아이스크림 드링크</a>
+              </li>
             </ul>
-            <ul>
-              <li><a href="">하겐다즈 소개</a></li>
-              <li><a href="">품질 좋은 원료</a></li>
+            <ul className="footer-list">
+              <li>
+                <a href="">하겐다즈 소개</a>
+              </li>
+              <li>
+                <a href="">품질 좋은 원료</a>
+              </li>
             </ul>
           </div>
           <div className="footer-bottom"></div>
