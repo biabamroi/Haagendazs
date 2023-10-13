9f8e85a800f15f9d74693138b58a99c08d0e9635
Janeee205 <ga473625@gmail.com>
2023-10-13 15:16:24 +0900

    🚧 section5작업중

---
+++ brand-app/src/components/Section5/Section5.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Section5.css'
+
+const Section5 = () => {
+  return (
+    <>
+      <section className='section5'>
+
+      </section>
+    </>
+  );
+};
+
+export default Section5;
