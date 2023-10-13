db1ee821dcac812a43e2f9243e6e2badb8cd77f7
choiseowu <chlthdud28@naver.com>
2023-10-13 13:02:26 +0900

    📝

--- brand-app/src/App.js
+++ brand-app/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 // import { Route, Routes, BrowserRouter } from 'react-router-dom';
 // import { Link, useLocation } from 'react-router-dom';
-import './App.css';
-import HeaderNav from './components/Header/HeaderNav.jsx';
-import Tabs from './components/Main/Tabs.jsx';
-import Membership from './components/Membership/Membership.jsx';
-import Footer from './components/Footer/Footer.jsx'
-import FooterNav from './components/Footer/FooterNav.jsx';
+import "./App.css";
+// import Loading from "./components/Loading/Loading.jsx";
+import HeaderNav from "./components/Header/HeaderNav.jsx";
+import Tabs from "./components/Main/Tabs.jsx";
+import Membership from "./components/Membership/Membership.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import FooterNav from "./components/Footer/FooterNav.jsx";
 
 function App() {
   return (
     <>
-    <div className='wrapper'>
-      <HeaderNav />
-      <Tabs />
-      <Membership />
-      <Footer />
-      <FooterNav />
-    </div>
+      <div className="wrapper">
+        {/* <Loading /> */}
+        <HeaderNav />
+        <Tabs />
+        <Membership />
+        <Footer />
+        <FooterNav />
+      </div>
     </>
   );
 }
