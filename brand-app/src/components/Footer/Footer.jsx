1d44cd68ba6eeb6f99f206bf118c1c52e7d3bf26
Janeee205 <ga473625@gmail.com>
2023-10-13 14:56:57 +0900

    ♻️

--- brand-app/src/components/Footer/Footer.jsx
+++ brand-app/src/components/Footer/Footer.jsx
@@ -9,36 +9,36 @@ const Footer = () => {
         <div className="footer-wrapper">
           <div className="footer-top">
             <div className="footer-left">
-              <img src="/assets/logo_white.png" alt="footer logo image" className="footerlogo" />
+              <img src="/assets/logo_white.png" alt="logo" className="footerlogo" />
               <button type="button">CONTACT US</button>
             </div>
 
             <div className="footer-right">
               <ul className="footer-list">
-                <li><a href="">파인트</a></li>
-                <li><a href="">미니 컵</a></li>
-                <li><a href="">스틱바</a></li>
-                <li><a href="">스틱바 멀티팩</a></li>
-                <li><a href="">미니 스틱바</a></li>
+                <li><a href="/">파인트</a></li>
+                <li><a href="/">미니 컵</a></li>
+                <li><a href="/">스틱바</a></li>
+                <li><a href="/">스틱바 멀티팩</a></li>
+                <li><a href="/">미니 스틱바</a></li>
               </ul>
 
               <ul className="footer-list">
-                <li><a href="">콘</a></li>
-                <li><a href="">콘 멀티백</a></li>
+                <li><a href="/">콘</a></li>
+                <li><a href="/">콘 멀티백</a></li>
               </ul>
 
               <ul className="footer-list">
-                <li><a href="">벌크 (업소용)</a></li>
-                <li><a href="">온라인 구매하기</a></li>
-                <li><a href="">업소문의</a></li>
-                <li><a href="">매장 소개</a></li>
-                <li><a href="">크리에이션 메뉴</a></li>
-                <li><a href="">아이스크림 드링크</a></li>
+                <li><a href="/">벌크 (업소용)</a></li>
+                <li><a href="/">온라인 구매하기</a></li>
+                <li><a href="/">업소문의</a></li>
+                <li><a href="/">매장 소개</a></li>
+                <li><a href="/">크리에이션 메뉴</a></li>
+                <li><a href="/">아이스크림 드링크</a></li>
               </ul>
 
               <ul className="footer-list">
-                <li><a href="">하겐다즈 소개</a></li>
-                <li><a href="">품질 좋은 원료</a></li>
+                <li><a href="/">하겐다즈 소개</a></li>
+                <li><a href="/">품질 좋은 원료</a></li>
               </ul>
             </div>
           </div>
@@ -47,13 +47,13 @@ const Footer = () => {
             <div className="fb-left">
               <p className="copyright">© 2023 General Mills. All Rights Reserved.</p>
               <ul>
-                <li><a href="">문의처</a></li>
-                <li><a href="">이용 약관</a></li>
-                <li><a href="">개인정보 취급방침</a></li>
-                <li><a href="">사이트 이용약관</a></li>
-                <li><a href="">쿠키 설정</a></li>
-                <li><a href="">사용자 지정</a></li>
-                <li><a href="">데이터 프라이버시 요청</a></li>
+                <li><a href="/">문의처</a></li>
+                <li><a href="/">이용 약관</a></li>
+                <li><a href="/">개인정보 취급방침</a></li>
+                <li><a href="/">사이트 이용약관</a></li>
+                <li><a href="/">쿠키 설정</a></li>
+                <li><a href="/">사용자 지정</a></li>
+                <li><a href="/">데이터 프라이버시 요청</a></li>
               </ul>
             </div>
 
