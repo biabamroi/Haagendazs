94f44a9ec4894cab7819c0611d180d85a3700d6d
Janeee205 <ga473625@gmail.com>
2023-10-13 16:27:47 +0900

    🚧 작업중

--- brand-app/src/components/Section5/Section5.jsx
+++ brand-app/src/components/Section5/Section5.jsx
@@ -7,20 +7,20 @@ const Section5 = () => {
     <>
       <section className='section5'>
         <div className="section5-wrapper">
-          <img src="/assets/section5.png" alt="이미지 화면" />
+          <img src="/assets/section5/section5.png" alt="이미지 화면" />
           <div className="text-section">
             <p className="s5-title">하겐다즈 품질</p>
             <div className="slider">
               <img className='slide-img' src="/assets/section5/small-vanilla.png" alt="본문 이미지" />
-              <p className='slide-text'>품질 좋은 다섯가지 원료만을 사용하여 만든
-                하겐다즈 바닐라  아이스크림.
-                깊고 부드러운 맛의 정수를 보여주는
+              <p className='slide-text'>품질 좋은 다섯가지 원료만을 사용하여 만든<br />
+                하겐다즈 바닐라 아이스크림.<br />
+                깊고 부드러운 맛의 정수를 보여주는<br />
                 순수한 맛을 경험해보세요.</p>
             </div>
-
+            <img src="" alt="" />
           </div>
         </div>
-      </section>
+      </section >
     </>
   );
 };
