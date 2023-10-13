9852540465f36392eee4f629fc4b20ece3b19e9d
Janeee205 <ga473625@gmail.com>
2023-10-13 16:08:42 +0900

    🚧 작업중

--- brand-app/src/components/Section5/Section5.jsx
+++ brand-app/src/components/Section5/Section5.jsx
@@ -7,11 +7,15 @@ const Section5 = () => {
     <>
       <section className='section5'>
         <div className="section5-wrapper">
-          <img src="/assets/section5.png" alt="이미지화면" />
+          <img src="/assets/section5.png" alt="이미지 화면" />
           <div className="text-section">
             <p className="s5-title">하겐다즈 품질</p>
             <div className="slider">
-              <img src="" alt="" />
+              <img className='slide-img' src="/assets/section5/small-vanilla.png" alt="본문 이미지" />
+              <p className='slide-text'>품질 좋은 다섯가지 원료만을 사용하여 만든
+                하겐다즈 바닐라  아이스크림.
+                깊고 부드러운 맛의 정수를 보여주는
+                순수한 맛을 경험해보세요.</p>
             </div>
 
           </div>
