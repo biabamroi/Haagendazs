5f20e81852bb449d9267f969e5b4345577689710
Janeee205 <ga473625@gmail.com>
2023-10-17 01:15:04 +0900

    ✨ section2 생성

---
+++ brand-app/src/components/Section2/Section2.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Section2.css'
+
+const Section2 = () => {
+    return (
+        <>
+            <section className='section2'>
+                <div className="section2-wrapper">
+
+
+
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Section2;
