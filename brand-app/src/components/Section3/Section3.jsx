87a6870b362d4d60a88568fcd62c3f94d0bc3c3f
Janeee205 <ga473625@gmail.com>
2023-10-16 16:41:14 +0900

    🚧 작업중

--- brand-app/src/components/Section3/Section3.jsx
+++ brand-app/src/components/Section3/Section3.jsx
@@ -6,30 +6,38 @@ const Section3 = () => {
   return (
     <>
       <section className="section3">
-        <div className="line-up fade-in-left"></div>
-        <div className="text">
-          <h1>하겐다즈 가치</h1>
-          <p>
-            비교할 수 없는 퀄리티의 아이스크림, 하겐다즈의 59년 역사를
-            확인해보세요
-          </p>
-        </div>
-        <div className="view">
-          <p>View More</p>
-          <Icon icon="solar:round-arrow-right-linear" height="30" />
-        </div>
-        <div className="image">
-          <img
-            src="/assets/section3/icecream2.png"
-            alt="icecream"
-            className="icecream"
-          />
-          <img
-            src="/assets/section3/icecream1.png"
-            alt="icecream"
-            className="ice-cream"
-          />
-          <div className="line-down fade-in-right"></div>
+        <div className="section3-wrapper">
+          <div className="text-top-area">
+            <div className="line-up fade-in-left"></div>
+            <p className="sec-title">하겐다즈 가치</p>
+          </div>
+
+          <div className="text-bottom-area">
+            <p className="desc">
+              비교할 수 없는 퀄리티의 아이스크림, 하겐다즈의 59년 역사를
+              확인해보세요
+            </p>
+
+            <button type="button" className="viewmore"><p>View More</p>
+              <Icon icon="solar:round-arrow-right-linear" height="30" />
+            </button>
+
+            <div className="line-down fade-in-right"></div>
+          </div>
+
+          <div className="image">
+            <img
+              src="/assets/section3/icecream2.png"
+              alt="icecream"
+              className="icecream"
+            />
+            <img
+              src="/assets/section3/icecream1.png"
+              alt="icecream"
+              className="ice-cream"
+            />
+          </div>
+
         </div>
       </section>
     </>
