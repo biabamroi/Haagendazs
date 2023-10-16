94e845c8428944acd1911440b4c1bfb4aaa20caa
Janeee205 <ga473625@gmail.com>
2023-10-16 13:17:34 +0900

    🚧 작업중

--- brand-app/src/components/Section5/Section5.jsx
+++ brand-app/src/components/Section5/Section5.jsx
@@ -27,7 +27,7 @@ const Section5 = () => {
           </div>
           {/* <img src="/assets/section5/section5.png" alt="이미지 화면" /> */}
 
-          <div className="text-section">
+          <div className="s5-text">
             <p className="s5-title">하겐다즈 품질</p>
             <div className="slider">
               <img className='slide-img' src="/assets/section5/small-vanilla.png" alt="본문 이미지" />
