aaea47bdb47aaaa7d0681d08b4b1552e2eb13a21
Janeee205 <ga473625@gmail.com>
2023-10-17 11:15:35 +0900

    💄 slick css추가

--- brand-app/src/components/Section5/Section5.jsx
+++ brand-app/src/components/Section5/Section5.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Section5.css'
-
+import "~slick-carousel/slick/slick.css";
+import "~slick-carousel/slick/slick-theme.css";
 
 const Section5 = () => {
   return (
