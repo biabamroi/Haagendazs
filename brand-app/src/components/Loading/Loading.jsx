773e7d8f8a6a098f2036f151de9089a32ce5ed87
Janeee205 <ga473625@gmail.com>
2023-10-17 16:06:49 +0900

    🚧 LoadingWave 작업중

--- brand-app/src/components/Loading/Loading.jsx
+++ brand-app/src/components/Loading/Loading.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Loading.css";
+import WaveSVG from "./Wave";
 
 const Loading = () => {
   return (
     <>
-      <div className="background">
-        <div className="line"></div>
+      <div className="loadingBackground">
         <div className="loading-wrapper">
-          <h1>Haagen-Daz</h1>
+          <h1>Haagen-Dazs</h1>
           <img src="/assets/icecream.png" alt="아이스크림이미지" />
+          <WaveSVG />
         </div>
       </div>
     </>
