094d46eb84f2c9d79d30cf1eaaafe96b2ec296f4
Kai <biabamroi@gmail.com>
2023-10-17 15:16:34 +0900

    🚧

--- brand-app/src/components/Section2/Section2.jsx
+++ brand-app/src/components/Section2/Section2.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Section2.css';
 import Wave from './Wave';
+import { Icon } from "@iconify/react";
 // import "~slick-carousel/slick/slick.css";
 // import "~slick-carousel/slick/slick-theme.css";
 
@@ -8,8 +9,22 @@ const Section2 = () => {
   return (
     <>
       <section className='section2'>
-        <div className="section2-wrapper">
           <Wave />
+        <div className="section2-wrapper">
+          <div className='circle-img'>
+            <img className='circle' src='/assets/section2/circle.png' alt="circle-icecream" />
+            <img className='icecreamcup' src="/assets/section2/iecle.png" alt="cup ice cream" />
+          </div>
+          <div className='section2-txt-wrap'>
+            <div className='section2-txt'>
+              <p>Best Products</p>
+              <p>하겐다즈 마카롱 레몬 & 유자</p>
+            </div>
+            <button type="button" className="viewmore">
+              <p>View More</p>
+              <Icon icon="solar:round-arrow-right-linear" height="30" />
+            </button>
+          </div>
 
 
 
