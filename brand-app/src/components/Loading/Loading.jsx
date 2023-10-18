442a987aec08b629420fe1dd76f9ce6b1b9adc49
Janeee205 <ga473625@gmail.com>
2023-10-18 15:01:07 +0900

    ♻️ 수정완료

--- brand-app/src/components/Loading/Loading.jsx
+++ brand-app/src/components/Loading/Loading.jsx
@@ -8,7 +8,9 @@ const Loading = () => {
     <>
       <div className="loadingBackground">
         <div className="loading-wrapper">
-          <img src="/assets/icecream.png" alt="아이스크림이미지" />
+          <div>
+            <img src="/assets/icecream.png" alt="아이스크림이미지" />
+          </div>
           <h1>Haagen-Dazs</h1>
           <WaveSVG />
         </div>
